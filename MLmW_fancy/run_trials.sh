6393f9804bf298d0c33e3515c298d50ae498081f
Elise Rosky <emrosky@mtu.edu>
2021-03-04 08:08:00 -0500

    ran all sims

--- MLmW_fancy/run_trials.sh
+++ MLmW_fancy/run_trials.sh
@@ -2,16 +2,16 @@
 # 
 
 
-TEMPERATURES_1=('286K' '287K' '288K' '289K' '290K' '291K' '292K' '293K')
-TEMPERATURES_500=('289K' '290K' '291K' '292K' '293K' '294K' '295K' '296K')
-TEMPERATURES_1000=('292K' '293K' '294K' '295K' '296K' '297K' '298K' '299K')
-WORKING_DIR=~/Freezing_Simulations/Melting/MLmW_fancy
+TEMPERATURES_1=(286 287 289 292 293)
+TEMPERATURES_500=(289 290 291 292 293 294 295 296)
+TEMPERATURES_1000=(292 293 294 295 296 297 298 299)
+WORKING_DIR=/data/emrosky-sim/Freezing_Simulations/Melting/MLmW_fancy
 
 # Run simulation for 1 atm
 for temp in "${TEMPERATURES_1[@]}"; do
-    echo $temp 
-    mkdir "${WORKING_DIR}/1atm"
+    echo $temp
     ./run_setup_scripts.sh 1atm $temp 1 ML
+    cd ${WORKING_DIR}
 done
 
 
@@ -20,6 +20,7 @@ for temp in "${TEMPERATURES_500[@]}"; do
     echo $temp 
     mkdir "${WORKING_DIR}/500atm"
     ./run_setup_scripts.sh 500atm $temp -500 ML
+    cd ${WORKING_DIR}
 done
 
 
@@ -28,6 +29,7 @@ for temp in "${TEMPERATURES_1000[@]}"; do
     echo $temp 
     mkdir "${WORKING_DIR}/1000atm"
     ./run_setup_scripts.sh 1000atm $temp -1000 ML
+    cd ${WORKING_DIR}
 done
 
 
