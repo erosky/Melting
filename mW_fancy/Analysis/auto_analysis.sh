d4d2de740c361c8fa8d644053135807c75c4165a
Elise Rosky <emrosky@mtu.edu>
2021-04-07 20:25:51 -0400

    mW melt temp analysis

--- mW_fancy/Analysis/auto_analysis.sh
+++ mW_fancy/Analysis/auto_analysis.sh
@@ -1,22 +1,52 @@
 #!/bin/bash
 
-TEMPERATURES=('286K' '287K' '288K' '289K' '290K' '291K' '292K' '293K')
-WORKING_DIR=~/Freezing_Simulations/Melting/Tmelt_1atm/Analysis_Scripts
+WORKING_DIR=~/Freezing_Simulations/Melting/mW_fancy/Analysis
 LOG_FILE=${WORKING_DIR}/auto_analysis.log
 DATETIME=$(date +"%Y-%m-%d_%H:%M:%S")
 
 # Log date and time
 echo "${DATETIME}" >> ${LOG_FILE}
 
-# Run simulation in each temperature directory
-for temp in "${TEMPERATURES[@]}"; do
-    cd "${WORKING_DIR}/../$temp" && \
-    # Check that input file exists before running simulation
-    if [ -f log.run_Tmelt_${temp}_AUTO ]; then
+TEMPERATURES_1=(270 271 272 273 274 275 276 277)
+TEMPERATURES_500=(271 272 273 274 275 276 277 278)
+TEMPERATURES_1000=(272 273 274 275 276 277 278 279)
+
+# Run analysis for 1 atm files
+for temp in "${TEMPERATURES_1[@]}"; do
+    cd "${WORKING_DIR}/../1atm/${temp}K" && \
+    # Check that input file exists before running analysis
+    if [ -f log.run_melt_${temp}K_1atm_mW ]; then
         echo "$temp input file found: Ready to analyze $temp simulation" >> ${LOG_FILE}
         # Run script, direct errors into log file
-        ${WORKING_DIR}/auto_analysis_run.sh log.run_Tmelt_${temp}_AUTO run_Tmelt_${temp}_AUTO LG
-        
+        ${WORKING_DIR}/auto_analysis_run.sh log.run_melt_${temp}K_1atm_mW run_melt_${temp}K_1atm_mW $temp $pres       
+    else
+        echo "Input file does not exist for $temp simulation" >> ${LOG_FILE}
+    fi
+done
+
+
+# Run analysis for -500 atm files
+for temp in "${TEMPERATURES_500[@]}"; do
+    cd "${WORKING_DIR}/../500atm/${temp}K" && \
+    # Check that input file exists before running analysis
+    if [ -f log.run_melt_${temp}K_-500atm_mW ]; then
+        echo "$temp input file found: Ready to analyze $temp simulation" >> ${LOG_FILE}
+        # Run script, direct errors into log file
+        ${WORKING_DIR}/auto_analysis_run.sh log.run_melt_${temp}K_-500atm_mW run_melt_${temp}K_500atm_mW $temp $pres       
+    else
+        echo "Input file does not exist for $temp simulation" >> ${LOG_FILE}
+    fi
+done
+
+
+# Run analysis for 1 atm files
+for temp in "${TEMPERATURES_1000[@]}"; do
+    cd "${WORKING_DIR}/../1000atm/${temp}K" && \
+    # Check that input file exists before running analysis
+    if [ -f log.run_melt_${temp}K_-1000atm_mW ]; then
+        echo "$temp input file found: Ready to analyze $temp simulation" >> ${LOG_FILE}
+        # Run script, direct errors into log file
+        ${WORKING_DIR}/auto_analysis_run.sh log.run_melt_${temp}K_-1000atm_mW run_melt_${temp}K_1000atm_mW $temp $pres       
     else
         echo "Input file does not exist for $temp simulation" >> ${LOG_FILE}
     fi
--- mW_fancy/Analysis/auto_plot_1000.sh
+++ mW_fancy/Analysis/auto_plot_1000.sh
@@ -1,36 +1,39 @@
 #!/bin/bash
 
 
-TEMPERATURES=('286K' '287K' '288K' '289K' '290K' '291K' '292K' '293K')
-WORKING_DIR=~/Freezing_Simulations/Melting/Tmelt_1atm/Analysis_Scripts
+TEMPERATURES=(272 273 274 275 276 277 278 279)
+
+WORKING_DIR=~/Freezing_Simulations/Melting/mW_fancy/Analysis
 LOG_FILE=${WORKING_DIR}/auto_plot.log
 DATETIME=$(date +"%Y-%m-%d_%H:%M:%S")
-TRIAL='LG'
+TRIAL='0'
+
 
 # Log date and time
 echo "${DATETIME}" >> ${LOG_FILE}
 
 for temp in "${TEMPERATURES[@]}"; do
-    data_file="../${temp}/run_Tmelt_${temp}_AUTO.dat"
+    data_file="../1000atm/${temp}K/run_melt_${temp}K_1000atm_mW.dat"
     if [ -f ${data_file} ]; then
         echo "$temp input file found: Ready to copy $temp data" >> ${LOG_FILE}
-        cp ${data_file} ${WORKING_DIR}/run_Tmelt_${temp}_AUTO.dat
+        cp ${data_file} ${WORKING_DIR}/run_melt_${temp}K_1000atm_mW.dat
     else
         echo "Input file does not exist for $temp simulation" >> ${LOG_FILE}
     fi
 done
 
+mkdir 1000atm
 
 # Plot and save Pot Energy
 gnuplot -e "set terminal png; \
-            set output 'Tmelt_PE_${TRIAL}.png';
+            set output '1000atm/Tmelt_PE_${TRIAL}.png';
             set title 'Potential Energy'; \
             set ylabel 'kJ/mol'; \
             set xlabel 'Time (ns)'; \
             set key outside ; \
             set key right top ; \
             set style data lines; \
-            plot for [i=286:293] 'run_Tmelt_'.i.'K_AUTO.dat' using 1:7 title ''.i.'K'; "
+            plot for [i=272:279] 'run_melt_'.i.'K_1000atm_mW.dat' using 1:7 title ''.i.'K'; "
 
 # Plot and save Total Energy
 gnuplot -e "set terminal png; \
@@ -40,7 +43,7 @@ gnuplot -e "set terminal png; \
             set key outside ; \
             set key right top ; \
             set style data lines; \
-            plot for [i=286:293] 'run_Tmelt_'.i.'K_AUTO.dat' using 1:5 title ''.i.'K'" > ${WORKING_DIR}/Tmelt_TE_${TRIAL}.png
+            plot for [i=272:279] 'run_melt_'.i.'K_1000atm_mW.dat' using 1:5 title ''.i.'K'" > ${WORKING_DIR}/1000atm/Tmelt_TE_${TRIAL}.png
 
 # Plot and save Mean square disp
 gnuplot -e "set terminal png; \
@@ -50,7 +53,7 @@ gnuplot -e "set terminal png; \
             set key outside ; \
             set key right top ; \
             set style data lines; \
-            plot for [i=286:293] 'run_Tmelt_'.i.'K_AUTO.dat' using 1:8 title ''.i.'K'" > ${WORKING_DIR}/Tmelt_MSD_${TRIAL}.png
+            plot for [i=272:279] 'run_melt_'.i.'K_1000atm_mW.dat' using 1:8 title ''.i.'K'" > ${WORKING_DIR}/1000atm/Tmelt_MSD_${TRIAL}.png
 
 # Plot and save Volume
 gnuplot -e "set terminal png; \
@@ -60,13 +63,13 @@ gnuplot -e "set terminal png; \
             set key outside ; \
             set key right top ; \
             set style data lines; \
-            plot for [i=286:293] 'run_Tmelt_'.i.'K_AUTO.dat' using 1:4 title ''.i.'K'" > ${WORKING_DIR}/Tmelt_VOL_${TRIAL}.png
+            plot for [i=272:279] 'run_melt_'.i.'K_1000atm_mW.dat' using 1:4 title ''.i.'K'" > ${WORKING_DIR}/1000atm/Tmelt_VOL_${TRIAL}.png
 
 
 for temp in "${TEMPERATURES[@]}"; do
-    if [ -f ${WORKING_DIR}/run_Tmelt_${temp}_AUTO.dat ]; then
+    if [ -f ${WORKING_DIR}/run_melt_${temp}K_1000atm_mW.dat ]; then
         echo "$temp input file found: Ready to remove $temp data" >> ${LOG_FILE}
-        rm ${WORKING_DIR}/run_Tmelt_${temp}_AUTO.dat
+        rm ${WORKING_DIR}/run_melt_${temp}K_1000atm_mW.dat
     else
         echo "Data file was not created for $temp simulation" >> ${LOG_FILE}
     fi
